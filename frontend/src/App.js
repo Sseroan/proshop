04537bc56d8bb14d398e404f41cb4a1ac90e1ac8
Ronald <sseroan@gmail.com>
2023-10-08 08:23:16 +0200

    Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
           <h1>Welcome to ProShop</h1>
         </Container>
       </main>
+      <Footer/>
     </>
   );
 };
