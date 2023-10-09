f52a6a89f7985003f87418cd2b60975e7372c9ef
Ronald <sseroan@gmail.com>
2023-10-09 05:37:44 +0200

    Updated Products

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to make calss while working.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    nunReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Iphone 11 Pro 256GB Memory ',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the Iphone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    nunReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Cannon E05 60D DSLR CAmera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs. The canon E05 60D DSLR further clarifies itself using a pair of robust focusing systems and an intuitive design.',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 15,
+    rating: 3,
+    nunReviews: 20,
+  },
+  {
+    _id: '4',
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with Playstation. Wether your into gaming, HD movies, television, or music.',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 7,
+    rating: 5,
+    nunReviews: 4,
+  },
+  {
+    _id: '5',
+    name: 'Logitec G-Seris Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle of your games with this Logitech LIGTSYNC gaming mouse. The six programmable buttons allow customisation for a smooth playing experience.',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 15,
+    rating: 3,
+    nunReviews: 20,
+  },
+  {
+    _id: '6',
+    name: 'Amazon Echo Bot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Bot - Our most popular smark speaker with a fabric design. It is out most compact smart speaker that fits perfectly into small space.',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    nunReviews: 12,
+  },
+];
+
+export default products;
