67588cd336f6e5e0d7431e8adadf0ffa8023c526
Ronald <sseroan@gmail.com>
2023-10-09 17:46:04 +0200

    Nodemon and concurrently

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,20 @@ import products from './data/products.js';
 
 const app = express();
 
-app.get('/', (req, res)=>{
-  res.status(200).send('API is running...')
-})
+app.get('/', (req, res) => {
+  res.status(200).send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
 
 const port = 5000;
 app.listen(port, () => {
-  console.log(`API os running on Port : ${port}`);
+  console.log(`API is running on Port : ${port}`);
 });
