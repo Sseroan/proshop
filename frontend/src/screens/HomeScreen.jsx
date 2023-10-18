856fdfe86b59c1faa527720510266ed37e27ec40
Ronald <sseroan@gmail.com>
2023-10-18 05:51:27 +0200

    Loader and Message

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,46 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Products from '../components/Products';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+// import axios from 'axios';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const { data } = await axios.get('/api/products');
+  //     setProducts(data);
+  //   };
 
-    fetchProducts();
-  }, []);
+  //   fetchProducts();
+  // }, []);
+
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Products product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error.data?.message || error.error}</Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Products product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
+      ;
     </>
   );
 };
